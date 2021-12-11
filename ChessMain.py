6a7a60fd3e89b5b1ff365cbaf4fa6bec3fbd8295
Muhammad Ahmed <ahmed495802@gmail.com>
2021-12-11 23:16:49 +0500

    Initial Setup Created

--- ChessMain.py
+++ ChessMain.py
@@ -1,1 +1,16 @@
-print("Chess Game")
+import pygame as pg
+import ChessEngine
+
+
+WIDTH = HEIGHT = 400
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+
+MAX_FPS = 15 # only from animation
+
+IMAGES = {}
+
+def load_images():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "wR", "wN", "wB", "wQ", "wK", "bP", "wP"]
+    for piece in pieces:
+        IMAGES[piece] = pg.transform.scale(pg.image.load("images/"+piece+".png"),(SQ_SIZE,SQ_SIZE))
