083d4c76cb869144a854c53a0b8e9e8eeaeac0f9
Faiq Shahzad <f03315558407@gmail.com>
2021-12-25 13:20:23 +0500

    Rook Moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -14,6 +14,9 @@ class GameState():
 
         self.whiteToMove = True
         self.moveLog=[]
+        self.possibleMoveFunctions = {'P': self.getPawnMoves, 'N': self.getKnightMoves, 'B': self.getBishopMoves, 'R': self.getRookMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
+
+    # Simple Chess Moves:
 
     def makeMove(self, move):
 
@@ -23,6 +26,119 @@ class GameState():
 
         self.moveLog.append(move)
         self.whiteToMove = not self.whiteToMove
+
+    def undoMove(self):
+
+        if len(self.moveLog) != 0:
+
+            move = self.moveLog.pop()
+
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove #switching the turn
+
+    #every possible move that a piece can make without the concern of other pieces
+    def getAllPossibleMoves(self):
+
+        possibleMoves = []
+
+        for row in range (len(self.board)):
+            for col in range (len(self.board[row])):
+                turn = self.board[row][col][0]
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
+                    piece = self.board[row][col][1]
+                    self.possibleMoveFunctions[piece](row, col, possibleMoves)
+
+        return possibleMoves
+
+
+    def getValidMoves(self):
+
+        return self.getAllPossibleMoves()
+
+    def getPawnMoves(self, row, col, possibleMoves):
+        # for white pieces
+
+        if self.whiteToMove:
+
+            # move up 1 or 2 squres
+            if self.board[row-1][col] == "--":
+                possibleMoves.append(Move((row, col), (row-1, col), self.board))
+                if row == 6 and self.board[row-2][col] == "--":
+                    possibleMoves.append(Move((row, col), (row-2, col), self.board))
+
+            # move diagonals
+            if col-1>=0:
+                if self.board[row-1][col-1][0] == 'b':
+                    possibleMoves.append(Move((row, col), (row-1, col-1), self.board))
+            if col+1 <=7:
+                if self.board[row-1][col+1][0] == 'b':
+                    possibleMoves.append(Move((row, col), (row-1, col+1), self.board))
+
+        # for black pieces
+
+        else:
+
+            # move up 1 or 2 squres
+            if self.board[row+1][col] == "--":
+                possibleMoves.append(Move((row, col), (row+1, col), self.board))
+                if row == 1 and self.board[row+2][col] == "--":
+                    possibleMoves.append(Move((row, col), (row+2, col), self.board))
+
+            # move diagonals
+            if col-1>=0:
+                if self.board[row+1][col-1][0] == 'w':
+                    possibleMoves.append(Move((row, col), (row+1, col-1), self.board))
+            if col+1<=7:
+                if self.board[row+1][col+1][0] == 'w':
+                    possibleMoves.append(Move((row, col), (row+1, col+1), self.board))
+
+
+
+    def getKnightMoves(self, row, col, possibleMoves):
+        # for white pieces
+        pass
+
+
+    def getBishopMoves(self, row, col, possibleMoves):
+        pass
+
+    def getRookMoves(self, row, col, possibleMoves):
+        # for white pieces
+
+        moveDirections = ((-1,0), (0,-1), (1,0), (0,1)) #up, left, down, right
+        if self.whiteToMove:
+            enemyColor = 'b'
+        else:
+            enemyColor = 'w'
+
+        for direction in moveDirections:
+            for i in range(1,8):
+                endRow = row + direction[0]*i
+                endCol = col + direction[0]*i
+                if 0<=endRow<=7 and 0<=endCol<=7:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":
+                        possibleMoves.append(Move((row, col), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColor:
+                        possibleMoves.append(Move((row, col), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
+
+
+        pass
+
+    def getQueenMoves(self, row, col, possibleMoves):
+        pass
+
+    def getKingMoves(self, row, col, possibleMoves):
+        pass
+
+
+
 
 
 class Move():
@@ -47,6 +163,12 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow*1000 + self.startCol*100 + self.endRow*10 + self.endCol
+
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
