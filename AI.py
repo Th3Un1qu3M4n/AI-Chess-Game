83ed3d59e5a201b0f4454ef539a07a6387ceb98f
Faiq Shahzad <f03315558407@gmail.com>
2021-12-28 14:57:24 +0500

    Recursive Mix Max

--- AI.py
+++ AI.py
@@ -3,7 +3,7 @@ import random
 pieceScore = {"K": 0, "Q": 10, "R": 5, "B": 3, "N": 3, "P": 1}
 CHECKMATE = 1000
 STALEMATE = 0
-depth = 2
+DEPTH = 2
 
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
@@ -50,7 +50,7 @@ def findBestMove(gs, validMoves):
 def findBestMinMaxMove(gs, validMoves):
     global nextMove
     nextMove = None
-    bestMinMaxMove(gs, validMoves, depth, gs.whiteToMove)
+    bestMinMaxMove(gs, validMoves, DEPTH, gs.whiteToMove)
     return nextMove
 
 def bestMinMaxMove(gs, validMoves, depth, whiteToMove):
@@ -58,7 +58,49 @@ def bestMinMaxMove(gs, validMoves, depth, whiteToMove):
     if depth == 0:
         return scoreMaterial(gs.board)
 
-# def scoreBoard(gs):
+    if whiteToMove:
+        maxScore = -CHECKMATE
+        for move in validMoves:
+            gs.makeMove(move)
+            nextMoves = gs.getValidMoves()
+            score = bestMinMaxMove(gs, nextMoves, depth - 1, False)
+            if score > maxScore:
+                maxScore = score
+                if depth == DEPTH:
+                    nextMove = move
+            gs.undoMove()
+        return maxScore
+    else:
+        minScore = CHECKMATE
+        for move in validMoves:
+            gs.makeMove(move)
+            nextMoves = gs.getValidMoves()
+            score = bestMinMaxMove(gs, nextMoves, depth - 1, True)
+            if score < minScore:
+                minScore = score
+                if depth == DEPTH:
+                    nextMove = move
+            gs.undoMove()
+        return minScore
+
+
+def scoreBoard(gs):
+
+    if gs.checkMate:
+        if gs.whiteToMove:
+            return -CHECKMATE
+        else:
+            return CHECKMATE
+    elif gs.staleMate:
+        return STALEMATE
+    score = 0
+    for row in gs.board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScore[square[1]]
+            if square[0] == 'b':
+                score -= pieceScore[square[1]]
+    return score
 
 
 def scoreMaterial(board):
