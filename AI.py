069ec7c91291d9ca6dcb2a513e962248b58d213b
Faiq Shahzad <f03315558407@gmail.com>
2021-12-28 18:15:20 +0500

    Recursive Mix Max Update

--- AI.py
+++ AI.py
@@ -8,44 +8,44 @@ DEPTH = 2
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
 
-def findBestMove(gs, validMoves):
-    if gs.whiteToMove:
-        turn = 1
-    else:
-        turn = -1
-
-    opponentMinMaxScore = CHECKMATE
-    bestPlayerMove = None
-    random.shuffle(validMoves)
-    for playerMove in validMoves:
-        gs.makeMove(playerMove)
-        opponentMoves = gs.getValidMoves()
-        if gs.checkMate:
-            opponentMaxScore = CHECKMATE
-        elif gs.staleMate:
-            opponentMaxScore = STALEMATE
-        else:
-            opponentMaxScore = -CHECKMATE
-            for opponentMove in opponentMoves:
-                gs.makeMove(opponentMove)
-                gs.getValidMoves()
-                if gs.checkMate:
-                    score = -turn * CHECKMATE
-                elif gs.staleMate:
-                    score = STALEMATE
-                else:
-                    score = -turn * scoreMaterial(gs.board)
-
-                if score > opponentMaxScore:
-                    opponentMaxScore = score
-                gs.undoMove()
-
-        if opponentMaxScore < opponentMinMaxScore:
-            opponentMinMaxScore = opponentMaxScore
-            bestPlayerMove = playerMove
-        gs.undoMove()
-
-    return bestPlayerMove
+# def findBestMove(gs, validMoves):
+#     if gs.whiteToMove:
+#         turn = 1
+#     else:
+#         turn = -1
+#
+#     opponentMinMaxScore = CHECKMATE
+#     bestPlayerMove = None
+#     random.shuffle(validMoves)
+#     for playerMove in validMoves:
+#         gs.makeMove(playerMove)
+#         opponentMoves = gs.getValidMoves()
+#         if gs.checkMate:
+#             opponentMaxScore = CHECKMATE
+#         elif gs.staleMate:
+#             opponentMaxScore = STALEMATE
+#         else:
+#             opponentMaxScore = -CHECKMATE
+#             for opponentMove in opponentMoves:
+#                 gs.makeMove(opponentMove)
+#                 gs.getValidMoves()
+#                 if gs.checkMate:
+#                     score = -turn * CHECKMATE
+#                 elif gs.staleMate:
+#                     score = STALEMATE
+#                 else:
+#                     score = -turn * scoreMaterial(gs.board)
+#
+#                 if score > opponentMaxScore:
+#                     opponentMaxScore = score
+#                 gs.undoMove()
+#
+#         if opponentMaxScore < opponentMinMaxScore:
+#             opponentMinMaxScore = opponentMaxScore
+#             bestPlayerMove = playerMove
+#         gs.undoMove()
+#
+#     return bestPlayerMove
 
 def findBestMove(gs, validMoves):
     global nextMove
